616fffaa741610c0b3b4f9769ed72395dc09699e
William Cooter <will@cooters.co.uk>
2019-01-30 13:12:22 +0000

    Started setting up back end
     - defined environment state and database credentials in .env
     - started setting up a connection to the database
     - created eslint and prettier configs

---
+++ src/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  NODE_ENV: process.env.NODE_ENV,
+  DB_HOST: process.env.DB_HOST,
+  DB_USER: process.env.DB_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_DATABASE: process.env.DB_DATABASE
+}
