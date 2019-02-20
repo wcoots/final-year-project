f0fa945554a17f92dcb466984df6d604282b83fd
William Cooter <will@cooters.co.uk>
2019-02-20 12:52:34 +0000

    testing stuff for netlify

--- frontend/src/components/SignUp.vue
+++ frontend/src/components/SignUp.vue
@@ -214,9 +214,9 @@ export default {
         }
     },
     created() {
-        if (localStorage.getItem('token') !== 'null') {
-            this.$router.push({ name: 'Dashboard' })
-        }
+        // if (localStorage.getItem('token') !== 'null') {
+        //     this.$router.push({ name: 'Dashboard' })
+        // }
     },
     computed: {
         isLoginDisabled() {
