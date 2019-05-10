7793e08de21734c3240c528bd2e521b2ecb8ba96
William Cooter <will@cooters.co.uk>
2019-05-10 16:07:48 +0100

    Final commit for the project

---
+++ front-end/src/components/Template.vue
@@ -0,0 +1,34 @@
+
+
+<template>
+  <div>
+    <el-header/>
+  </div>
+</template>
+
+<script>
+import Header from './Header'
+
+export default {
+    name: 'Template',
+    components: {
+        Header,
+    },
+    data() {
+        return {
+            user: null,
+        }
+    },
+    created() {},
+    mounted() {},
+    computed: {},
+    methods: {},
+    watch: {},
+    destroyed() {},
+}
+</script>
+
+<style>
+</style>
+
+
